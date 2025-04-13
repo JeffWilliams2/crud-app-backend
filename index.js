f90723cfede1606cbdb0c17b6a68b95505a60434
jeffwilliams2 <68485116+JeffWilliams2@users.noreply.github.com.>
2025-04-13 16:45:35 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Routes
+app.use("/api/products", productRoute);
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello from Node API Server!");
+});
+
+
+mongoose
+    .connect(
+        "mongodb+srv://admin:PJ7ulQU4IU9Jv7LG@backenddb.addokgq.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB"
+    )
+.then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+})
+.catch(() => {
+    console.log("Connection failed!");
+});
